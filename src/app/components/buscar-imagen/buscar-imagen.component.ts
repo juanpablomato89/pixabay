b55d285deb0ac7565e1659e65d3093ff9736b235
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-06-08 13:44:34 -0400

    add input and button

--- src/app/components/buscar-imagen/buscar-imagen.component.ts
+++ src/app/components/buscar-imagen/buscar-imagen.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarImagenComponent implements OnInit {
 
+  nombreImagen = '';
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  buscarImagenes() { }
 
 }
